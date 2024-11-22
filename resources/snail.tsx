bf47934acfcd02c478ceabbff322742c9e0e9f83
cali-kennedy <calikennedy81@gmail.com>
2024-11-21 18:22:32 -0600

    Working on new large map

--- resources/snail.tsx
+++ resources/snail.tsx
@@ -1,4 +1,29 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="snail" tilewidth="32" tileheight="32" tilecount="25" columns="5">
  <image source="snail.png" width="160" height="160"/>
+ <tile id="20">
+  <animation>
+   <frame tileid="0" duration="300"/>
+   <frame tileid="1" duration="300"/>
+   <frame tileid="2" duration="300"/>
+   <frame tileid="3" duration="300"/>
+   <frame tileid="4" duration="300"/>
+   <frame tileid="5" duration="300"/>
+   <frame tileid="6" duration="300"/>
+   <frame tileid="7" duration="300"/>
+   <frame tileid="8" duration="300"/>
+   <frame tileid="9" duration="300"/>
+   <frame tileid="10" duration="300"/>
+   <frame tileid="11" duration="300"/>
+   <frame tileid="12" duration="300"/>
+   <frame tileid="13" duration="300"/>
+   <frame tileid="14" duration="300"/>
+   <frame tileid="15" duration="300"/>
+   <frame tileid="16" duration="300"/>
+   <frame tileid="17" duration="300"/>
+   <frame tileid="18" duration="300"/>
+   <frame tileid="19" duration="300"/>
+   <frame tileid="20" duration="300"/>
+  </animation>
+ </tile>
 </tileset>
