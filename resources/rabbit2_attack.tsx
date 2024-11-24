0a151be4379bbbbc20696a917a0e3ef7fc557046
cali-kennedy <calikennedy81@gmail.com>
2024-11-23 23:04:54 -0600

    Added fightscreen animations and sounds

---
+++ resources/rabbit2_attack.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="rabbit2_attack" tilewidth="128" tileheight="128" tilecount="6" columns="2">
+ <image source="rabbit2_attack.png" width="256" height="384"/>
+ <tile id="4">
+  <animation>
+   <frame tileid="0" duration="300"/>
+   <frame tileid="1" duration="300"/>
+   <frame tileid="2" duration="300"/>
+   <frame tileid="3" duration="300"/>
+   <frame tileid="4" duration="300"/>
+  </animation>
+ </tile>
+</tileset>
