204e598dd21cbd9028bffc8555237323f1b5fa77
cali-kennedy <calikennedy81@gmail.com>
2024-11-25 17:41:37 -0600

    Created new NPC Character

---
+++ resources/squirrel.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="squirrel" tilewidth="64" tileheight="64" tilecount="6" columns="2">
+ <image source="squirrel.png" width="128" height="192"/>
+ <tile id="5">
+  <animation>
+   <frame tileid="0" duration="300"/>
+   <frame tileid="1" duration="300"/>
+   <frame tileid="2" duration="300"/>
+   <frame tileid="3" duration="300"/>
+   <frame tileid="4" duration="300"/>
+   <frame tileid="5" duration="300"/>
+  </animation>
+ </tile>
+</tileset>
